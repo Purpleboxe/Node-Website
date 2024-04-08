f187b58ab5917e7a7235c9d5c820f41656f8c6f8
Antonio Zamora <antoniozamora21331@gmail.com>
2024-04-07 18:46:44 -0700

    Make changes to index.js using express and other packages as well as add CSS

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
-const http = require("http");
-const fs = require("fs");
+const express = require("express");
+const path = require("path");
 
 const port = 8080;
+const app = express();
 
-const server = http.createServer((req, res) => {
-  res.write("<h1>Hello World!</h1>");
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/css", express.static(path.join(__dirname, "public/css")));
+
+app.get("/", sendFileWrapper("index.html"));
+app.get("/about", sendFileWrapper("about.html"));
+app.get("/contact-me", sendFileWrapper("contact-me.html"));
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
-server.listen(port, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+app.use((err, req, res, next) => {
+  console.error("Error:", err);
+  res.status(500).send("Internal Server Error");
 });
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
+
+function sendFileWrapper(fileName) {
+  return (req, res) => {
+    const filePath = path.join(__dirname, "views", fileName);
+    res.status(200).sendFile(filePath, (err) => {
+      if (err) {
+        next(err);
+      }
+    });
+  };
+}
